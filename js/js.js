c3231dbc42c50d1aa79c8ee2b48e9e3b48c5acef
Astral <81131964+ravadon@users.noreply.github.com>
2023-03-24 14:42:45 +0300

    8

--- js/js.js
+++ js/js.js
@@ -49,11 +49,12 @@ function OnLoad()
             replace(/DatabaseObject variable/g, "<a href='#preparingBlueprintDiv'><span class='object'>DatabaseObject</span></a> variable").
             replace(/OnComplete/g, "<a href='Media/OnCompleteEvent.png' target='_blank'><span class='event'>OnComplete</span></a>").
             replace(/On Complete/g, "<a href='Media/OnCompletePin.png' target='_blank'><span class='delegate'>On Complete</span></a>").
+            replace(/ExecuteStatement/g, "<a href='Media/ExecuteStatementRaw.png' target='_blank'><span class='function'>ExecuteStatement</span></a>").
             replace(/GetAffectedRowsCount/g, "<a href='Media/GetAffectedRowsCountRaw.png' target='_blank'><span class='getFunction'>GetAffectedRowsCount</span></a>").
             replace(/GetColumnValuesByColumnName/g, "<a href='Media/GetColumnValuesByColumnName.png' target='_blank'><span class='function'>GetColumnValuesByColumnName</span></a>").
             replace(/Print String node/g, "<a href='Media/PrintStringNode.png' target='_blank'><span class='function'>Print String</span></a> node").
              
-            replace(/Statement Tag/g, "<a href='Media/StatementStringPin.png' target='_blank'><span class='fname'>StatementTag</span></a>").
+            replace(/Statement Tag/g, "<a href='Media/StatementStringPin.png' target='_blank'><span class='fname'>Statement Tag</span></a>").
             replace(/TRUE/g, "<span class='true'>TRUE</span>").
             replace(/FALSE/g, "<span class='false'>FALSE</span>")
             
