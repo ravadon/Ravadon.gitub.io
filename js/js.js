2511d8cbd01d17b199526332d2e11889a67ff25c
Astral <81131964+ravadon@users.noreply.github.com>
2023-03-28 01:25:27 +0300

    22

--- js/js.js
+++ js/js.js
@@ -5,12 +5,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
  document.addEventListener('readystatechange', event => { 
 
-    // When HTML/DOM elements are ready:
-    // if (event.target.readyState === "interactive") {   //does same as:  ..addEventListener("DOMContentLoaded"..
-    //     alert("hi 1");
-    // }
-
-    // When window loaded ( external resources are loaded too- `css`,`src`, etc...) 
     if (event.target.readyState === "complete") {
         OnLoad();
     }
@@ -25,22 +19,12 @@ function ReplaceText(element, textToReplace, replaceWith)
 
 function OnLoad()
 {
-    //alert("hi 2");
-    //document.getElementsByTagName('p').innerHTML = document.getElementsByTagName('p').innerHTML.replace('%Set%','John');
-    //document.getElementById('name').innerHTML = document.getElementById('name').innerHTML.replace('%Name%','John');
-
-    
     var allParas = document.getElementsByTagName('p');
 
     for(var i = 0; i < allParas.length; i++)
     {
         if (allParas[i].classList.contains("para"))
         {
-            // let text = allParas[i].innerHTML;
-            // let result = text.replace(/Connection String Pin/g, "<a href='Media/ConnectionStringPin.png' target='_blank'><span class='string'>Connection String Pin</span></a>");
-            // allParas[i].innerHTML = result;
-
-           
             let text = allParas[i].innerHTML;
             let result = text.replace(/Connection String Pin/g, "<a href='Media/ConnectionStringPin.png' target='_blank'><span class='string'>Connection String</span></a> pin").
             replace(/Connection String variable/g, "<a href='#preparingBlueprintDiv'><span class='string'>Connection String</span></a> variable").
@@ -61,38 +45,6 @@ function OnLoad()
 
             
             allParas[i].innerHTML = result;
-
-           
-            // ReplaceText(allParas[i], /Connection String Pin/g, "<a href='Media/ConnectionStringPin.png' target='_blank'><span class='string'>Connection String Pin</span></a>").
-            // ReplaceText(allParas[i], /Connection String Variable/g, "<a href='Media/ConnectionStringPin.png' target='_blank'><span class='string'>Connection String Pin</span></a>")
-
-            // text = allParas[i].innerHTML;
-            // result = text.replace(/Connection String Variable/g, "<a href='#preparingBlueprintDiv.png' target='_blank'><span class='string'>Connection String Variable</span></a>");
-            // allParas[i].innerHTML = result;
         }
-
-
-        
-        // <a href="preparingBlueprintDiv"><span class="object">DatabaseObject</span></a>;
-        // <span class="error">Success</span>;
-        // <span class="error">FALSE</span>;
-
-        // <span class="string">In String</span> to <span class="string">Error Msg</span> pin on the <span class="event">OnComplete </span> event we made</p>;
-
-        // <span class="success">TRUE</span>;
-        // <span class="string">Column Name</span> pin;
-        // <span class="string">Password pin</span>
-        // <span class="string">Return Value</span>
-        // <a href="#preparingBlueprintDiv"><span class="string">ColumnValues</span></a> variable</a>
-        // <span class="string">String</span>
-        // <span class="object">DatabaseObject</span>
-        // <span class="string">ArrayOfString</span>
-
-
-
-
-        //alert("hi 2");
-
-        
     }
 }
